69fa01d19ab866ccbdd6743990ae9a1f42c3dba2
eyyVince <54894603+DreaKal@users.noreply.github.com>
2025-05-21 17:03:17 +0800

    dowhileLoop.java

---
+++ dowhileLoop.java
@@ -0,0 +1,10 @@
+public class dowhileLoop {
+  public static void main(String[] args){
+    int i = 1;
+    do{
+      System.out.println("Numbers: " + i);
+      i++;
+    } while( i <= 5 );
+    }
+  }
+}
